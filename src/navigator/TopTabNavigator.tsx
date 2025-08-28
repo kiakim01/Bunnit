518dd6e7aeb4215ba377dcfbfacd2becad83bcb0
kiakim01 <hnmpot0318@naver.com>
2025-08-28 21:22:22 +0900

    Fix. TopTabNavigator UI custom

--- src/navigator/TopTabNavigator.tsx
+++ src/navigator/TopTabNavigator.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import FirstTabView from '../screens/top-tabs/FirstTabView.tsx';
 import SecondTabView from '../screens/top-tabs/SecondTabView.tsx';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import ThirdTabView from '../screens/top-tabs/ThirdTabView.tsx';
 
-const Tab = createMaterialTopTabNavigator();
+// const Tab = createMaterialTopTabNavigator();
 
 const TopTabNavigator = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabs = [
+    { name: '식단', count: 0 },
+    { name: '운동', count: 0 },
+    { name: '신체', count: 0 },
+  ];
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case 0:
+        return <FirstTabView />;
+      case 1:
+        return <SecondTabView />;
+      case 2:
+        return <ThirdTabView />;
+      default:
+        return null;
+    }
+  };
   return (
     <View className={'flex-1 w-full'}>
       {/* 드래그 핸들 */}
@@ -15,25 +35,34 @@ const TopTabNavigator = () => {
         <View className={'w-10 h-1 bg-gray-300 rounded-full'} />
       </View>
 
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#111827',
-          tabBarInactiveTintColor: '#9CA3AF',
-          tabBarIndicatorStyle: {
-            backgroundColor: '#50BDFA',
-          },
-          tabBarLabelStyle: { fontSize: 16, fontWeight: '500' },
-          tabBarStyle: {
-            shadowOpacity: 0,
-            elevation: 0,
-            borderWidth: 0,
-          },
-        }}
-      >
-        <Tab.Screen name={'식단'} component={FirstTabView} />
-        <Tab.Screen name={'운동'} component={SecondTabView} />
-        <Tab.Screen name={'신체'} component={ThirdTabView} />
-      </Tab.Navigator>
+      <View className="flex-1 bg-white">
+        {/* 커스텀 탭 헤더 */}
+        <View className="flex justify-center items-center flex-row bg-slate-50  border-b border-gray-100">
+          {tabs.map((tab, index) => (
+            <TouchableOpacity
+              key={index}
+              className={`flex-1 items-center justify-center py-2 px-2 rounded-lg mx-1 ${
+                activeTab === index
+                  ? 'bg-white border border-slate-200'
+                  : 'bg-transparent'
+              }`}
+              onPress={() => setActiveTab(index)}
+            >
+              <Text
+                className={`text-center text-base font-medium mb-1 ${
+                  activeTab === index ? 'text-gray-900' : 'text-gray-500'
+                }`}
+              >
+                {tab.name} {'  '}
+                {tab.count}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        {/* 탭 콘텐츠 */}
+        <View className="flex-1">{renderTabContent()}</View>
+      </View>
     </View>
   );
 };
