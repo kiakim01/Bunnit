53c2fbd2567c25c03092ca1966f622b22efd18ce
kiakim01 <hnmpot0318@naver.com>
2025-08-29 10:30:25 +0900

    Refactor. WeeklyDateItem

--- src/component/WeeklyDateItem.tsx
+++ src/component/WeeklyDateItem.tsx
@@ -18,33 +18,31 @@ const WeeklyDateItem: React.FC<WeeklyDateItemProps> = ({
   isToday,
   onPress,
 }) => {
+  const getDayNameColor = () => {
+    if (dayOfWeek === 0) return 'text-red-500';
+    if (dayOfWeek === 6) return 'text-blue-500';
+    return 'text-gray-600';
+  };
+
   return (
     <TouchableOpacity onPress={() => onPress(day)} className="mx-2">
-      <View className={`w-16 h-20 items-center justify-center rounded-2xl `}>
-        <Text
-          className={`text-xs mb-1 ${
-            dayOfWeek === 0
-              ? 'text-red-500'
-              : dayOfWeek === 6
-              ? 'text-blue-500'
-              : 'text-gray-600'
-          }`}
-        >
-          {dayName}
-        </Text>
-        <View
-          className={`${
-            isSelected &&
-            'w-10 h-10 items-center justify-center rounded-full border-2 border-blue-500 '
-          }`}
-        >
-          <Text className={`text-lg  text-gray-800 ${isToday && 'font-bold'}`}>
-            {day}
-          </Text>
+        <View className={`w-16 h-20 items-center justify-center rounded-2xl `}>
+          <Text className={`text-xs mb-1 ${getDayNameColor()}`}>{dayName}</Text>
+          <View
+            className={`${
+              isSelected &&
+              'w-10 h-10 items-center justify-center rounded-full border-2 border-blue-500 '
+            }`}
+          >
+            <Text
+              className={`text-lg  text-gray-800 ${isToday && 'font-bold'}`}
+            >
+              {day}
+            </Text>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
-  );
+      </TouchableOpacity>
+    );
 };
 
 export default WeeklyDateItem;
